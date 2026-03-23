454330939df65f90e400252d470ca5eeee6f6130
Thanawat-Pete <petelov3dinosaurth@gmail.com>
2026-03-23 16:03:01 +0700

    feat: Implement initial API service setup and environment variable examples for client and server.

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://final-exam-23-03-2026.onrender.com/api",
   withCredentials: true, // Crucial for cookie-based auth
 });
 
